b5d2e763aeb0390e66a0251e65543b219d704bd0
Chris Uzoh <genbliz@gmail.com>
2021-03-30 00:36:07 +0100

    updated regex query

--- src/couch/couch-filter-query-operation.ts
+++ src/couch/couch-filter-query-operation.ts
@@ -223,7 +223,7 @@ export class CouchFilterQueryOperation {
 
   private operation__filterBeginsWith({ fieldName, term }: { fieldName: string; term: any }): IQueryConditions {
     const result = {
-      [fieldName]: { $regex: `^${term}` },
+      [fieldName]: { $regex: `(?i)^${term}` },
     } as IQueryConditions;
     return result;
   }
@@ -277,7 +277,7 @@ export class CouchFilterQueryOperation {
             //
           } else if (conditionKey === "$beginsWith") {
             const result = {
-              [`${fieldName}.${subFieldName}`]: { $regex: `^${val}` },
+              [`${fieldName}.${subFieldName}`]: { $regex: `(?i)^${val}` },
             } as IQueryConditions;
             results.push(result);
           } else {
